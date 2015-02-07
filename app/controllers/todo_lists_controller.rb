804d8915843476b6266ec5b2f5a47b99fc47bea2
Kelly Strickland <imloved@embarqmail.com>
2015-02-07 15:51:31 -0500

    Show all todo list items.

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,10 +1,13 @@
 class TodoListsController < ApplicationController
 	def index
 		@todo_list = TodoList.new
+		@all_todos = TodoList.all
 	end
 
 	def create
 		@todo_list = TodoList.create(params.require(:todo_list).permit(:description))
   	redirect_to root_url
   end
+
+  
 end
